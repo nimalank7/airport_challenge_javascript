bdd20fc4c9e4dfb5052dabdd2fa5f191eb6337e3
Dana Gajewska <mdgajewska@outlook.com>
2018-11-26 15:54:16 +0000

    Completes FizzBuzz in JavaScript

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -8,4 +8,22 @@ describe ("Fizzbuzz", function() {
       expect(fizzbuzz.play(3)).toEqual('Fizz');
     });
   });
+
+  describe('multiples of 5', function() {
+    it('buzzes for 5', function() {
+      expect(fizzbuzz.play(5)).toEqual('Buzz');
+    });
+  });
+
+  describe('multiples of 15', function() {
+    it('fizzbuzzes for 15', function() {
+      expect(fizzbuzz.play(15)).toEqual('FizzBuzz');
+    });
+  });
+
+  describe('returns number if not divisible by 3 or 5', function() {
+    it('returns number', function() {
+      expect(fizzbuzz.play(7)).toEqual(7);
+    });
+  });
 });
--- jasmine/src/fizzbuzz.js
+++ jasmine/src/fizzbuzz.js
@@ -1,5 +1,18 @@
 function FizzBuzz() {
   this.play = function(number) {
-    return 'Fizz';
+    if (number % 15 == 0) {
+      return 'FizzBuzz';
+    } else if (number % 3 == 0) {
+      return 'Fizz';
+    } else if (number % 5 == 0) {
+      return 'Buzz';
+    } else {
+      return number;
+    }
   }
+}
+
+var fizzBuzz = new FizzBuzz();
+for (var i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.play(i));
 }
