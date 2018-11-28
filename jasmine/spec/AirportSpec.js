a8e93a3db454c9400e75596ce5f85336bd5177cf
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-11-28 13:55:56 +0000

    added random integer script as well as stormy conditions

--- jasmine/spec/AirportSpec.js
+++ jasmine/spec/AirportSpec.js
@@ -1,17 +1,59 @@
 describe ("Airport", function() {
   var airport;
+  var weather;
   beforeEach(function(){
     airport = new Airport();
+    plane = jasmine.createSpyObj('plane',['status'])
   });
-  describe('sets up capacity to empty array', function (){
-    it('responds true', function() {
+  describe('attempts to land plane', function (){
+    it('sets up capacity to empty true responds true', function() {
       expect(airport.capacity).toEqual([]);
     });
+    it('responds true when sunny', function() {
+      airport.weather = "sunny"
+      airport.land(plane);
+      expect(airport.capacity.includes(plane)).toBe(true);
+    });
+    it('responds false when stormy', function() {
+      airport.weather = "stormy"
+      expect(airport.land(plane)).toBe(false);
+    });
   });
-  describe('lands plane and stores in capacity array', function (){
-    it('responds true', function() {
-      airport.land("Plane");
-      expect(airport.capacity.includes("Plane")).toBe(true);
+  describe('checks whether airport is empty when empty', function (){
+    it('and responds true', function() {
+      expect(airport.isEmpty()).toBe(true);
+    });
+    it('and responds false', function() {
+      airport.weather = "sunny"
+      airport.capacity.push(plane)
+      expect(airport.isEmpty()).toBe(false);
+    });
+  });
+  describe('attempts to take off plane', function (){
+    it('and returns false if empty', function() {
+      airport.weather = "sunny"
+      expect(airport.takeOff()).toBe(false);
+    });
+    it('and returns true if not empty', function() {
+      airport.weather = "sunny"
+      airport.capacity.push(plane)
+      expect(airport.takeOff()).toBe(true);
+    });
+    it('and returns true if sunny', function() {
+      airport.weather = "sunny"
+      airport.land(plane)
+      expect(airport.takeOff()).toBe(true);
+    });
+    it('and returns false if stormy', function() {
+      airport.weather = "stormy"
+      airport.capacity.push(plane)
+      expect(airport.takeOff()).toBe(false);
+    });
+  });
+  describe('weather', function (){
+    it('sets random weather to stormy or sunny', function() {
+      weather = ["stormy", "sunny"]
+      expect(weather.includes(airport.weather)).toBe(true);
     });
   });
 });
--- jasmine/src/airport.js
+++ jasmine/src/airport.js
@@ -1,7 +1,28 @@
 function Airport(){
   this.capacity = []
-}
-Airport.prototype.land = function(plane) {
-  this.capacity.push(plane);
-  return true;
+  this.weather = (function(){
+    return ["stormy", "sunny"][getRandomIntInclusive(0, 1)];
+  }());
+
+  this.land = function(plane) {
+    if(this.weather == "stormy"){
+      return false;
+    } else {
+      this.capacity.push(plane);
+      return true;
+    }
+  };
+
+  this.isEmpty = function() {
+    return (this.capacity.length == 0 ? true : false);
+  };
+
+  this.takeOff = function() {
+    if(this.isEmpty() == false && this.weather != "stormy"){
+      this.capacity.pop
+      return true;
+    } else {
+      return false;
+    }
+  };
 }
